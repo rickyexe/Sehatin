da4b51d3b11521358f0bb2057744674abd0ae68d
Ricky <53333122+rickyexe@users.noreply.github.com>
2019-12-03 23:46:36 +0700

    update minor

--- app/src/main/java/com/ricky/sehatin/DetailRestaurantActivity.kt
+++ app/src/main/java/com/ricky/sehatin/DetailRestaurantActivity.kt
@@ -28,12 +28,6 @@ class DetailRestaurantActivity : AppCompatActivity() {
         val intent = this.intent
         val id = intent.getIntExtra(id, 0)
 
-//        val editor = sharedPref.edit()
-//        editor.putInt("idresto", idresto)
-//        editor.apply()
-
-
-//        Toast.makeText(this, id.toString(), Toast.LENGTH_SHORT).show()
 
         val q = Volley.newRequestQueue(this)
         val url = "http://riset.group/penir/penir_d8/viewdetailrestaurant.php"
@@ -174,156 +168,15 @@ class DetailRestaurantActivity : AppCompatActivity() {
     }
 
 
-//    override fun onRestart() {
-//        super.onRestart()
-//
-//        setContentView(R.layout.activity_detail_restaurant)
-//
-//        val pref: SharedPreferences = this.getSharedPreferences("login", Context.MODE_PRIVATE)
-//
-//        val idrestaurant = pref.getInt("idresto", 0)
-//        val id = idrestaurant
-//
-////        Toast.makeText(this, id.toString(), Toast.LENGTH_SHORT).show()
-//
-//        val q = Volley.newRequestQueue(this)
-//        val url = "http://riset.group/penir/penir_d8/viewdetailrestaurant.php"
-//        val url2 = "http://riset.group/penir/penir_d8/viewkomentarlist.php"
-//        val stringRequest = object :
-//            StringRequest(Request.Method.DEPRECATED_GET_OR_POST, url, Response.Listener<String>
-//            { response ->
-//                try {
-//                    val obj = JSONObject(response)
-//                    val arr = obj.getJSONArray("restoran")
-//
-//                    for (i in 0 until arr.length()) {
-//                        val restaurant = arr.getJSONObject(i)
-//                        txtNama.text = restaurant.getString("nama_restaurant")
-//                        txtAlamat.text = restaurant.getString("alamat_restaurant")
-//                        txtJam.text = restaurant.getString("jam_buka")
-//                        val image = restaurant.getString("foto")
-//                        Picasso.with(applicationContext).load(image).resize(416, 202).into(imgResto)
-//
-//                        btnMaps.setOnClickListener {
-//                            val i = Intent()
-//                            val destination = restaurant.getString("nama_restaurant")
-//
-//
-//                            i.action = Intent.ACTION_VIEW
-//                            i.data = Uri.parse("geo:0,0?q=$destination")
-//
-//                            if (i.resolveActivity(packageManager) != null) {
-//                                startActivity(i)
-//                            }
-//
-//                        }
-//
-//                        btnPhone.setOnClickListener {
-//
-//                            val phoneNumber = restaurant.getString("telepon")
-//                            val intent = Intent(Intent.ACTION_DIAL).apply {
-//                                data = Uri.parse("tel:$phoneNumber")
-//                            }
-//                            if (intent.resolveActivity(packageManager) != null) {
-//                                startActivity(intent)
-//                            }
-//
-//                        }
-//
-//                    }
-//
-//
-//                } catch (e: JSONException) {
-//                    Toast.makeText(
-//                        this, e.message.toString(),
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//
-//            },
-//                Response.ErrorListener {}) {
-//            override fun getParams(): Map<String, String> {
-//                val params = HashMap<String, String>()
-//                params.put("id", id.toString())
-//                return params
-//            }
-//        }
-//
-//        q.add(stringRequest)
-//
-//        val stringRequest2 = object :
-//            StringRequest(Request.Method.DEPRECATED_GET_OR_POST, url2, Response.Listener<String>
-//            { response ->
-//                try {
-//
-//                    if(response == "failed")
-//                    {
-//                        txtTidakAda.text = "Belum Ada Review"
-//
-//                    }
-//                    else {
-//
-//
-//                        val obj = JSONObject(response)
-//                        val arr = obj.getJSONArray("komentar")
-//
-//
-//
-//
-//                        komen = ArrayList<Review>()
-//                        for (i in 0 until arr.length()) {
-//                            val rev = arr.getJSONObject(i)
-//                            komen.add(
-//                                Review(
-//                                    rev.getString("username"),
-//                                    rev.getString("tanggal"),
-//                                    rev.getString("komentar")
-//                                )
-//                            )
-//                        }
-//                        val arrayAdapter2 = ReviewCustomAdapter(this, komen)
-//                        listrev.adapter = arrayAdapter2
-//                    }
-//
-//
-//                } catch (e: JSONException) {
-//                    Toast.makeText(
-//                        this, e.message.toString(),
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//
-//            },
-//                Response.ErrorListener {}) {
-//            override fun getParams(): Map<String, String> {
-//                val params = HashMap<String, String>()
-//                params.put("id", id.toString())
-//                return params
-//            }
-//        }
-//
-//        q.add(stringRequest2)
-//
-//
-//
-//
-//
-//
-//
-//        fabtambahkomen.setOnClickListener {
-//
-//
-//            val homeIntent = Intent(this, TambahKomentarActivity::class.java)
-//            homeIntent.putExtra(TambahKomentarActivity.idrestoran, id )
-//            startActivity(homeIntent)
-//
-//
-//        }
-//
-//
-//    }
+override fun onRestart() {
 
+    val intent = intent
+    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+    finish()
+    startActivity(intent)
 
-    }
+    super.onRestart()
+
+    }}
 
 
--- app/src/main/java/com/ricky/sehatin/FragmentProfile.kt
+++ app/src/main/java/com/ricky/sehatin/FragmentProfile.kt
@@ -4,13 +4,23 @@ package com.ricky.sehatin
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_detail_restaurant.*
 import kotlinx.android.synthetic.main.fragment_fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_fragment_profile.view.*
+import org.json.JSONException
+import org.json.JSONObject
 
 
 /**
@@ -29,7 +39,7 @@ class FragmentProfile : Fragment() {
 
         val pref: SharedPreferences = this.activity!!.getSharedPreferences("login", Context.MODE_PRIVATE)
 
-        val idd = pref.getInt("id", 0)
+        val iduser = pref.getInt("id", 0)
 
         vieww.btnLogout.setOnClickListener {
             val preference = pref.edit()
@@ -40,6 +50,51 @@ class FragmentProfile : Fragment() {
             homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(homeIntent)
         }
+
+        Toast.makeText(context, iduser.toString(), Toast.LENGTH_LONG).show()
+
+        val q = Volley.newRequestQueue(context)
+        val url = "http://riset.group/penir/penir_d8/profile.php"
+        val stringRequest = object :
+            StringRequest(
+                Request.Method.DEPRECATED_GET_OR_POST, url, Response.Listener<String>
+                { response ->
+                    try {
+                        val obj = JSONObject(response)
+                        val arr = obj.getJSONArray("profile")
+
+                        for (i in 0 until arr.length()) {
+                            val profile = arr.getJSONObject(i)
+
+                            txtJumlahReview.text = profile.getInt("komentar").toString()
+                            txtProfile.text = profile.getString("username")
+
+
+                        }
+
+
+
+
+
+
+
+                    } catch (e: JSONException) {
+                        Toast.makeText(
+                            context, e.message.toString(),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                },
+                Response.ErrorListener {}) {
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                params.put("iduser", iduser.toString())
+                return params
+            }
+        }
+
+        q.add(stringRequest)
 
 
 
